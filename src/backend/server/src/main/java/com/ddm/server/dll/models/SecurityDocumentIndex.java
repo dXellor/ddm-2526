a70a7e73dced1a59cf32beaa0f4540fb1557eecb
N. Simic <nikola7simic@gmail.com>
2026-02-24 19:40:51 +0100

    fix: remove custom naming mapping because es serializer is cringe

--- src/backend/server/src/main/java/com/ddm/server/dll/models/SecurityDocumentIndex.java
+++ src/backend/server/src/main/java/com/ddm/server/dll/models/SecurityDocumentIndex.java
@@ -15,46 +15,47 @@ import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 @Data
 @Document(indexName = "security_documents")
 @Setting(settingPath = "index-config.json")
+@JsonSerialize
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SecurityDocumentIndex {
 
     @Id
-    @Field(type = FieldType.Keyword, name = "id")
+    @Field(type = FieldType.Keyword)
     private String id;
 
-    @Field(type = FieldType.Text, name = "full_name", store = true)
+    @Field(type = FieldType.Text, store = true)
     private String fullName;
 
-    @Field(type = FieldType.Text, name = "org_name", store = true)
+    @Field(type = FieldType.Text, store = true)
     private String orgName;
 
-    @Field(type = FieldType.Text, name = "org_address", store = true)
+    @Field(type = FieldType.Text, store = true)
     private String orgAddress;
 
-    @Field(type = FieldType.Text, name = "threat_name", store = true)
+    @Field(type = FieldType.Text, store = true)
     private String threatName;
 
-    @Field(type = FieldType.Text, name = "threat_description", store = true)
+    @Field(type = FieldType.Text, store = true)
     private String threatDescription;
 
-    @Field(type = FieldType.Keyword, name = "threat_level", store = true)
+    @Field(type = FieldType.Keyword, store = true)
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     private ThreatLevel threatLevel;
 
-    @Field(type = FieldType.Text, name = "threat_sample_hash", store = true)
+    @Field(type = FieldType.Text, store = true)
     private String threatSampleHash;
 
-    @Field(type = FieldType.Text, analyzer = "serbian", searchAnalyzer = "serbian", name = "document_content")
+    @Field(type = FieldType.Text, analyzer = "serbian", searchAnalyzer = "serbian")
     private String documentContent;
 
     @GeoPointField
-    @Field(name = "geo_point", store = true)
+    @Field(store = true)
     private GeoPoint geoPoint;
 
     @JsonSerialize(using = ToStringSerializer.class)
-    @Field(name="vectorized_content", type = FieldType.Dense_Vector, dims = 768, similarity = "cosine")
+    @Field(type = FieldType.Dense_Vector, dims = 768, similarity = "cosine")
     private float[] vectorizedContent;
 
-    @Field(type = FieldType.Long, name = "system_id", store = true)
+    @Field(type = FieldType.Long, store = true)
     private Long systemId;
 }
